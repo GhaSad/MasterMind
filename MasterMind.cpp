25c4d69460d5fc8fd24dc9495c405c913e791187
Ghali <91273543+GhaSad@users.noreply.github.com>
2023-04-19 17:32:57 +0200

    Add files via upload

--- MasterMind.cpp
+++ MasterMind.cpp
@@ -3,25 +3,23 @@
 #include <string>
 #include <conio.h>
 
-const int taille_max = 10;
+const int taille_max = 4;
 using tab = std::array<std::string,taille_max>;
 
 struct rangee
 {
     tab t;
-    int n; //taille du tableau et de la rangée
 };
 
 void init_jeu(rangee &r, int &nb_tentatives, int &nb_couleurs)
 {
     std::cout<<"Combien de tentatives ?"; std::cin>>nb_tentatives;
     std::cout<<"Combien de couleur ? "; std::cin>>nb_couleurs;
-    std::cout<<"Taille des rangées ? " ; std::cin>>r.n
 }
 
 void init_tab(rangee &r)
 {
-    for(int i=0 ; i < r.n ; ++i)
+    for(int i=0 ; i < taille_max ; ++i)
     {
         r.t[i]="";
     }
@@ -35,11 +33,44 @@ void saisie_joueurs(std::string &j1, std::string &j2)
 
 void saisie_code(std::string &code, std::string j1, rangee r)
 {
-    std::cout<<j1<<" est en train de saisir le code couleur : ";
-    while(code.length() != r.n)
+    std::cout<<j1<<" est en train de saisir le code secret : ";
+    while(code.length() != taille_max)
     {
         code += getch();
     }
+    std::cout<<std::endl;
+}
+
+bool verif(std::string code, std::string reponse)
+{
+    if(reponse.length()!=code.length())
+        return false;
+    else if(reponse != code)
+        return false;
+    else return true;
+}
+
+void jeu(std::string code, std::string j2, std::string j1, int nb_tentatives)
+{
+    int tour=0;
+    std::string reponse;
+    while(tour<=nb_tentatives)
+    {
+        tour++;
+        std::cout<<j2<<" a vous de jouer : "; std::cin>>reponse;
+        if(verif(code,reponse))
+        {
+            std::cout<<j2<<" a gagne en "<<tour<<" tours !";
+            return;
+        }
+        else
+        {
+            std::string indice;
+            std::cout<<"reponse incorrecte, "<<j1<<" va saisir l'indice :";
+            std::cin>>indice;
+        }
+    }
+    std::cout<<j2<<" a perdu ! "<<j1<<" est donc le gagnant de cette manche ";
 }
 
 int main()
@@ -49,7 +80,8 @@ int main()
     init_tab(r);
     std::string j1, j2;
     saisie_joueurs(j1,j2);
-    std::string code;
+    std::string code; int tour=0;
     saisie_code(code,j1,r);
+    jeu(code,j2,j1,nb_tentatives);
     return 0;
 }
