90bb96dd89f89f6fef0a15564f510a374f263f6b
Ghali <91273543+GhaSad@users.noreply.github.com>
2023-05-26 16:44:05 +0200

    Add files via upload

--- MasterMindFinal.cpp
+++ MasterMindFinal.cpp
@@ -1,20 +1,22 @@
 #include "mastermindV1.h"
 #include "mastermindV2.h"
+#include "mastermindV3.h"
 #include <string>
 #include <iostream>
 
 int main()
 {
     int choix;
-    std::cout<<"Bienvenu dans le jeu MasterMind !"<<std::endl;
-    std::cout<<"Il y a deux versions : "<<std::endl<<std::endl;
+    std::cout<<"Bienvenue dans le menu du jeu MasterMind !"<<std::endl;
+    std::cout<<"Il y a trois versions : "<<std::endl<<std::endl;
     std::cout<<"Une version a deux joueurs, avec un score et un gagnant a la fin "<<std::endl;
     std::cout<<"Une deuxieme ou le joueur doit deviner le code genere par l'ordinateur ."<<std::endl;
-    std::cout<<"A quelle version voulez-vous jouer ? (1-2)"<<std::endl;
+    std::cout<<"Et en dernier c'est la version INTELLIGENCE ARTIFICIELLE, ou l'ordinateur devra deviner votre code."<<std::endl;
+    std::cout<<"A quelle version voulez-vous jouer ? (1-2-3)"<<std::endl;
     std::cin>>choix;
-    while(choix!=1 and choix!=2 )
+    while(choix!=1 and choix!=2  and choix !=3)
     {
-        std::cout<<"Erreur veuillez choisir 1 ou 2 ."<<std::endl;
+        std::cout<<"Erreur veuillez choisir 1 ou 2 ou 3 ."<<std::endl;
         std::cin>>choix;
     }
     if(choix==1)
@@ -48,7 +50,6 @@ int main()
         else
             std::cout<<"Les deux joueurs sont a egalite !!"<<std::endl;
     }
-
     else if(choix==2)
     {
         std::string j1, code; int nb_couleurs, nb_manches, nb_tentatives, manche_gagnee=0, manche=0;
@@ -64,6 +65,58 @@ int main()
             std::cout<<"Felicitations, vous avez gagne la partie avec "<<manche_gagnee<<" manches gagnees ! "<<std::endl;
         else 
             std::cout<<"Dommage ! Vous avez perdu la partie avec "<<nb_manches-manche_gagnee<<"manches perdues ! "<<std::endl;
+    }
+    else if(choix==3)
+    {
+        int tour = 1;
+        bool gagne = false;
+
+        std::cout<<"Saisissez le code : "<<std::endl;
+        code=saisie_code();
+        tentative = {1, 1, 2, 2}; 
+
+        //Création des 1296 combinaisons possibles
+        genere_combinaisons(combinaisons);
+        solutionsProbables.insert(solutionsProbables.end(), combinaisons.begin(), combinaisons.end());
+
+        std::cout << "Code : ";
+        intTostring(code);
+        std::cout <<" "<< std::endl;
+        std::cout << std::endl;
+
+        while (!gagne) 
+        {
+
+            //On supprime la tentative des solutions possibles
+            supprime_code(combinaisons, tentative);
+            supprime_code(solutionsProbables, tentative);
+
+            //On veut obtenir l'indice de la tentative
+            indice3 = indication(tentative, code);
+
+            std::cout << "Tour : " << tour << std::endl;
+            std::cout << "Tentative : ";
+            intTostring(tentative);
+            std::cout << " = " << indice << std::endl;
+
+            //Si l'indice c'est 4 pions noirs, alors le jeu est fini
+            if (indice3 == "BBBB") {
+                gagne = true;
+                std::cout << "L'ordinateur a trouve la combinaison !" << std::endl;
+                break;
+            }
+
+            //On supprime des solutions probables tout code qui ne donnerait pas la meme réponse s'il était le code
+            triCodes(solutionsProbables, tentative, indice3);
+
+            //On calcule le score minmax
+            tentativeSuivantes = minmax();
+
+            //On choisis la tentative suivante
+            tentative = tentativeSuivante(tentativeSuivantes);
+
+            tour++;
+        }
     }
     return 0;
 }
